6723642e43fffd47c3365557b8c79bcb296a3a9d
ducdm87 <ducdm87@gmail.com>
2015-11-17 15:17:44 +0700

    2015.11.17  user frontend: sửa file router

--- protected/apps/frontend/users/router.php
+++ protected/apps/frontend/users/router.php
@@ -10,23 +10,9 @@ function fnHelperFindMenuUser($view, $layout) {
     $YiiMenu = YiiMenu::getInstance();
     $menuItem = $YiiMenu->getMenuApp("users");
     $found = false;
-
     foreach ($menuItem as $item) {
         $params = $item['params'];
         if ($params->view == $view AND $params->layout == $layout) {
-            return $item['id'];
-        }
-    }
-    return false;
-} 
- 
-
-function fnHelperFindMenuUsers() {
-    $YiiMenu = YiiMenu::getInstance();
-    $menuItem = $YiiMenu->getMenuApp("users");
-    foreach ($menuItem as $item) {
-        $params = $item['params'];
-        if ($params->view == "home") {
             return $item['id'];
         }
     }
@@ -41,44 +27,23 @@ function usersBuildRoute(& $query) {
         if(!isset($query['layout']))  $query['layout'] = "";
         if($menuID = fnHelperFindMenuUser($query['view'], $query['layout'])){
                 $query['menuID'] = $menuID;
-        }        
-    }
-        
-    unset($query['view']);   
-    unset($query['layout']);   
+        }else{
+            $segments[] = $query['view'];
+            $segments[] = $query['layout'];
+            unset($query['view']);   
+            unset($query['layout']);  
+        }      
+    }     
     return $segments;
 }
 
 // $segments: array path from url[0: tin-tuc,1:tin-lam-dep ... ]
 function usersParseRoute($segments, $_params = null) {
     $n = count($segments);
-    $params = array();
-    $segment = array_pop($segments);
-
-    if ($_params == null) {
-        if ($n == 1) {
-            $params['view'] = "category";
-            $params['alias'] = $segment;
-            $params['id'] = intval($segment);
-        } else {
-            $params['view'] = "detail";
-            $params['id'] = intval($segment);
-        }
-    } else {
-
-        if ($_params->view == "category") {
-            $params['view'] = "detail";
-            $params['id'] = intval($segment);
-        } else if ($_params->view == "home") {
-            if ($n == 1) {
-                $params['view'] = "category";
-                $params['alias'] = $segment;
-                $params['id'] = intval($segment);
-            } else {
-                $params['view'] = "detail";
-                $params['id'] = intval($segment);
-            }
-        }
-    }
+    $params = array(); 
+     
+    $params['view'] = $segments[0];
+    $params['layout'] = $segments[1];
+    
     return $params;
 }
